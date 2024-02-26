b300965c2d3e14884b869702221c4fe102db5e41
UlanTursunbek <ulantursunbekdev@gmail.com>
2024-02-26 22:48:18 +0600

    Add quick acces menu button

---
+++ src/feature/quickAddMenu/ui/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/shared/components/ui/button"
+
+export const QuickAddMenu = () => {
+  return (
+    <Button
+      variant="default"
+      //   size="lg"
+      className="rounded-full h-14 w-14 bg-yellow-500 text-3xl flex justify-center items-center p-0 m-0"
+    >
+      +
+    </Button>
+  )
+}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,30 +1,32 @@
 "use client"
 
-import { Fragment } from "react"
 import Link from "next/link"
 
+import { QuickAddMenu } from "@/feature"
 import { LINKS } from "@/shared/constants/links"
 
 export const Navbar = () => {
   return (
-    <div className="grid grid-cols-5 gap-4 justify-center w-full rounded-t-2xl bg-zinc-900 p-5">
+    <div className="grid grid-cols-5 gap-4 justify-center w-full rounded-t-2xl bg-zinc-900">
       {LINKS.slice(0, 2).map((link, index) => (
         <Link
           key={index}
           href={link.url}
-          className="flex justify-center text-white align-middle h-9"
+          className="flex justify-center text-white align-middle h-12 p-2"
         >
           {link.icon}
         </Link>
       ))}
 
-      <div className="bg-yellow-400">23</div>
+      <div className="flex justify-center -translate-y-2">
+        <QuickAddMenu />
+      </div>
 
       {LINKS.slice(2).map((link, index) => (
         <Link
           key={index}
           href={link.url}
-          className="flex justify-center text-white align-middle h-9"
+          className="flex justify-center text-white align-middle h-12 p-2"
         >
           {link.icon}
         </Link>
