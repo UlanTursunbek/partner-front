a46a61e08ae8e9f7edb5c0907f7e7a8d551a9e1a
UlanTursunbek <ulantursunbekdev@gmail.com>
2024-02-26 21:02:54 +0600

    Add shadcn/ui to project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compilerOptions: {
+    baseUrl: "/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
